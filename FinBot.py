17e1c693623c000b8708ead58f64df12b0858ec4
Ayush Mehta <ayush.mehta1702@gmail.com>
2024-09-14 16:19:11 +0200

    update in files

--- FinBot.py
+++ FinBot.py
@@ -2,8 +2,8 @@ import streamlit as st
 from langchain_community.utilities import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
 from langchain_community.chat_models import ChatOpenAI
-from chat_ui02 import chat_interface
-from UI01 import chat_interface
+
+from Chatbot_UI import chat_interface
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -27,22 +27,3 @@ def initialize_sql_chain():
 sql_chain = initialize_sql_chain()
 
 chat_interface(sql_chain)
-
-# # streamlit Setup
-# st.title("Finance Bot")
-#
-# st.write("Connected to the database. You can now ask questions about the financial data!")
-#
-# query = st.text_input("Enter your question:")
-#
-# if query:
-#     try:
-#         sql_query = sql_chain.run(query)
-#
-#         st.write(f"**Answer:** {sql_query}")
-#     except Exception as e:
-#         st.error(f"An error occurred: {str(e)}")
-#         st.write("Debugging info:")
-#         st.write(f"Query: {query}")
-#         st.write(f"Type of query: {type(query)}")
-#         st.write("Ensure that `sql_chain.run()` is correctly handling the input.")
