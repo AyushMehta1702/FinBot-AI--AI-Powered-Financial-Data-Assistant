6c46649f61f531d2c3276ff72be27c66feaffe73
Ayushkumar Mehta <62299047+AyushMehta1702@users.noreply.github.com>
2024-09-21 14:05:37 +0200

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, LargeBinary
 
-#engine = create_engine(dbname="finance_chatbot", user="postgres", password="001127", host="localhost", port=5432)
-engine = create_engine('postgresql://postgres:001127@localhost/finance_chatbot')
+
+engine = create_engine('postgresql://postgres:password@localhost/dbname')
 metadata = MetaData()
 
 financial_data = Table(
